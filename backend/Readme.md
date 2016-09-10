ca43370dcf38453c75771d7e4b9c8295bfd4ac55
David Calle <davidcalle9430@gmail.com>
2016-09-10 12:20:51 -0500

    Orquestación entre contenedores terminada

---
+++ backend/Readme.md
@@ -0,0 +1,1 @@
+# Backend en un API REST
---
+++ backend/clubmat-backend/src/test/java/com/prosofi/clubmat/ClubmatBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prosofi.clubmat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClubmatBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
